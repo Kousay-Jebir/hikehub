4bb0eea3167af7620a818ff08fa4834a1491dd33
Kousay Jebir <kousayjebir@gmail.com>
2024-07-21 21:48:50 +0100

    basic auth service to integrate with backend api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import SignIn from './auth/components/SignIn';
+import SignUp from './auth/components/SignUp';
 
 function App() {
+  const isSignin = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        isSignin ? <SignIn></SignIn> : <SignUp></SignUp>
+      }
     </div>
   );
 }
