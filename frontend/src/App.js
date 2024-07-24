9b462491f755d648eddb8af08a195b93d548ddbb
Kousay Jebir <kousayjebir@gmail.com>
2024-07-24 11:06:41 +0100

    implementing navbar ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,15 @@ import SignUp from './auth/components/SignUp';
 import ProfileSetupStepper from './profile-management/profile-setup/ProfileSetupStepper';
 import OrganizationProfileSetup from './profile-management/profile-setup/OrganizationProfileSetup';
 import ProfileSettings from './profile-management/profile-settings/ProfileSettings';
+import NavBar from './shared/navbar/NavBar';
+import { CssBaseline } from '@mui/material';
 
 function App() {
   const isSignin = true;
   return (
     <div className="App">
+      <CssBaseline/>
+      <NavBar></NavBar>
       {
         isSignin ? <SignIn></SignIn> : <SignUp></SignUp>
       }
---
+++ frontend/src/shared/navbar/NavBar.js
@@ -0,0 +1,160 @@
+import React, { useState } from 'react';
+import {
+  AppBar,
+  Box,
+  Container,
+  Toolbar,
+  Typography,
+  IconButton,
+  Menu,
+  MenuItem,
+  Avatar,
+  Button,
+  TextField,
+  Tooltip,
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import AdbIcon from '@mui/icons-material/Adb';
+import AuthContext from '../../auth/context/AuthContext';
+
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+  const authData = React.useContext(AuthContext);
+  const { isLoggedIn } = authData;
+  const pages = isLoggedIn ? ['Feed', 'Recommendations'] : ['Login', 'Signup'];
+  const settings = ['Profile', 'Account', 'Logout'];
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="fixed">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            HikeHub
+          </Typography>
+
+          <Box sx={{ display: 'flex', flexGrow: 1, alignItems: 'center' }}>
+            <TextField
+              placeholder="Search..."
+              variant="outlined"
+              size="small"
+              InputProps={{ sx: { bgcolor: 'background.paper' } }}
+              sx={{ width: '100%', maxWidth: 400, ml: 2 }}
+            />
+          </Box>
+
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+              {pages.map((page) => (
+                <Button key={page} sx={{ mx: 1, color: 'inherit' }}>
+                  {page}
+                </Button>
+              ))}
+            </Box>
+
+            {authData.isLoggedIn ? <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenUserMenu}
+              color="inherit"
+              sx={{ ml: 1 }}
+            >
+              <Avatar alt="User Avatar" src="/static/images/avatar/2.jpg" />
+            </IconButton> : null }
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  {setting}
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          <IconButton
+            size="large"
+            aria-label="open drawer"
+            edge="end"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+            sx={{ display: { xs: 'flex', md: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{
+              display: { xs: 'block', md: 'none' },
+            }}
+          >
+            {pages.map((page) => (
+              <MenuItem key={page} onClick={handleCloseNavMenu}>
+                {page}
+              </MenuItem>
+            ))}
+          </Menu>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default ResponsiveAppBar;
